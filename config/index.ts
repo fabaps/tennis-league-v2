35496ca7ceb127fed2cc188f2034a382abbefcf6
alexsantos-eth <alexdsantosv@gmail.com>
2025-03-24 17:24:11 -0600

    [ Add ] env variables

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,10 @@
-export const SOUNDS = false;
-export const PRESS_ANY_KEY_TO_START = false;
-export const MOTION_BACKGROUND = false;
-export const GENERATIVE_BACKGROUND = true;
+export const SOUNDS = process.env.SOUNDS || false;
+export const PRESS_ANY_KEY_TO_START =
+  process.env.PRESS_ANY_KEY_TO_START || false;
+export const MOTION_BACKGROUND = process.env.MOTION_BACKGROUND || false;
+export const GENERATIVE_BACKGROUND = process.env.GENERATIVE_BACKGROUND || false;
 export const API_URL =
+  process.env.API_URL ||
   "https://rank-meme-backend-production.up.railway.app/api";
-export const SHARE_DOMAIN = "https://rank-meme-frontend.vercel.app";
+export const SHARE_DOMAIN =
+  process.env.SHARE_DOMAIN || "https://rank-meme-frontend.vercel.app";
