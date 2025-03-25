4efcae26c5beca81bb565ac5fa4ed221544f2246
alexsantos-eth <alexdsantosv@gmail.com>
2025-03-24 18:59:28 -0600

    [ Fix ] og title

--- app/profile/[wallet]/page.tsx
+++ app/profile/[wallet]/page.tsx
@@ -1,36 +1,28 @@
 "use server";
 
-import ProfileView from "@/views/profile";
-import getOGTitle from "@/views/profile/tools";
 import { Metadata } from "next";
 
-type Params = Promise<{ wallet: string }>;
+import ProfileView from "@/views/profile";
 
-interface Props {
-  params: {
-    wallet: string;
-    rank?: string | number;
-  };
-}
+type Params = Promise<{ wallet: string }>;
+type Props = {
+  params: Params;
+};
 
 const NEXT_PUBLIC_SHARE_DOMAIN = process.env.NEXT_PUBLIC_SHARE_DOMAIN;
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { wallet, rank } = params;
+  const { wallet } = await params;
   const ogURL = new URL(`${NEXT_PUBLIC_SHARE_DOMAIN}/api/og`);
   ogURL.searchParams.set("wallet", wallet);
-  const title = getOGTitle({ rank: rank ?? "??" });
 
   return {
-    title,
     description: `Wallet ${wallet} is making moves on MemeRank. Check out their current position in the leaderboard.`,
     openGraph: {
       images: [ogURL.toString()],
-      title,
       description: `MemeRank's leaderboard status for wallet ${wallet}`,
     },
     twitter: {
       card: "summary_large_image",
-      title,
       description: `🔥 See how wallet ${wallet} ranks among the top meme lords on MemeRank.`,
       images: [ogURL.toString()],
     },
