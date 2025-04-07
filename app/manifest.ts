39f1e0e6bee111187c1e4ea11cf1ef0b56408ee1
alexsantos-eth <alexdsantosv@gmail.com>
2025-04-07 12:06:39 -0600

    [ Fix ] manifest icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#245A4C",
     icons: [
       {
-        src: "/images/icons/icon-192-maskable.png",
+        src: "images/icons/icon-192-maskable.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/images/icons/icon-512-maskable.png",
+        src: "images/icons/icon-512-maskable.png",
         sizes: "512x512",
         type: "image/png",
       },
