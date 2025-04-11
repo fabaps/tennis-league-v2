9c8d463c1d697d5587bf7e7eeec4fd60f7c76938
kevin del cid <danieldelcidf@gmail.com>
2025-04-10 18:35:32 -0600

    Se integraron los cambios referentes a entornos de desarrollo y produccion

--- config/index.ts
+++ config/index.ts
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRYHl8HNR7NIdEvx8_4koAacl8TztDkyc",
-  authDomain: "tennis-league-gt.firebaseapp.com",
-  projectId: "tennis-league-gt",
-  storageBucket: "tennis-league-gt.firebasestorage.app",
-  messagingSenderId: "563145632335",
-  appId: "1:563145632335:web:c82d4c99a93ebf95f4183a",
-  measurementId: "G-1DTJRH69VP",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,6 +18,17 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export { db, auth, storage };
+if (process.env.NODE_ENV === "development") {
+
+  console.log("🔥 Conectando Firestore al emulador...");
+  connectFirestoreEmulator(db, "localhost", 8080);
+
+  console.log("🔑 Conectando Auth al emulador...");
+  connectAuthEmulator(auth, "http://localhost:9099");
+
+  console.log("📦 Conectando Storage al emulador...");
+  connectStorageEmulator(storage, "localhost", 9199);
+}
 
 export default app;
+export { db, auth, storage };
